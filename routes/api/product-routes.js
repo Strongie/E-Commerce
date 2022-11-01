4b359751697e0477c1e4fef658f2f7b9564d3050
Strongie <nickstrong665@gmail.com>
2022-11-01 21:44:54 +1100

    finish product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
     include:[{
         model: Category,
         attributes: ['category_name']
@@ -28,6 +29,27 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne(
+    {
+      attributes: ['id', 'product_name', 'price', 'stock'],
+   where: { 
+        id: req.params.id
+      },
+      include:[{
+        model: Category,
+        attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+  }
+]
+
+  })
+  .then((productData) => {
+    res.json(productData);
+  })
+  .catch((err) => res.json(err));
 });
 
 // create new product
@@ -40,7 +62,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    tagIds: req.body.tagIds    
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -106,6 +133,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;

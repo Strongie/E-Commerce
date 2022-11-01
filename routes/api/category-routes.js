1541c81bbd89e8ad651ca936196f5d634d8e65e4
Strongie <nickstrong665@gmail.com>
2022-11-01 19:15:33 +1100

    finish category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,37 +42,45 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+
+})
+
+.then((categoryData) => {
+  res.json(categoryData)
+  .catch((err) => res.json(err));
+})
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
 
-  Category.update(
-    {
-      // All the fields you can update and the data attached to the request body.
-      title: req.body.title,
-      author: req.body.author,
-      isbn: req.body.isbn,
-      pages: req.body.pages,
-      edition: req.body.edition,
-      is_paperback: req.body.is_paperback,
-    },
-    {
-     
-      where: {
+  Category.update(req.body,
+   {
+   where: {
         id: req.params.id,
       },
     }
   )
     .then((updatedCategory) => {
-      // Sends the updated book as a json response
-      res.json(updatedCategory);
+    res.json(updatedCategory);
     })
     .catch((err) => res.json(err));
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
